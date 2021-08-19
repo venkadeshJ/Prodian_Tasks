7a8e2cd761455f25c307a52ddea79675d1be39d3
venkadeshJ <venkadeshjpm@gmail.com>
2021-08-19 13:45:53 +0530

    edited

---
+++ plugins-libraries/bootstrap-5.1.0-dist/js/datatable.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    var eventFired = function ( type ) {
+        var n = $('#demo_info')[0];
+        n.innerHTML += '<div>'+type+' event - '+new Date().getTime()+'</div>';
+        n.scrollTop = n.scrollHeight;      
+    }
+ 
+    $('#example')
+        .on( 'order.dt',  function () { eventFired( 'Order' ); } )
+        .on( 'search.dt', function () { eventFired( 'Search' ); } )
+        .on( 'page.dt',   function () { eventFired( 'Page' ); } )
+        .DataTable();
+} );
